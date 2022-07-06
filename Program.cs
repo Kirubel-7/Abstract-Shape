fcbe7607ea06d9376c6720c61ad00bdf630ab645
Kirubel Esayas <107740002+Kirubel-7@users.noreply.github.com>
2022-07-06 21:41:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,128 +1,144 @@
-﻿Console.WriteLine("Select a Shape:");
-Console.WriteLine("1.Circle");
-Console.WriteLine("2.Triangle");
-Console.WriteLine("3.Square");
-Console.WriteLine("4.Rectangle");
-int x=Convert.ToInt32(Console.ReadLine());
-switch (x) 
+﻿string input, i;
+do
 {
-    case 1:
-        {
-            Circle circle = new Circle();
-            Console.WriteLine("Enter Z radius:");
-            //double r=Convert.ToDouble(Console.ReadLine());
-            //Circle circle = new Circle(r);
-            circle._rad = Console.ReadLine();    
-            
-            Console.WriteLine("Calculate:");
-            Console.WriteLine("1.Area");
-            Console.WriteLine("2.Perimeter");
-            int i = Convert.ToInt32(Console.ReadLine());
-                    switch (i) 
-                    {
-                        case 1:
-                           {
-                        Console.WriteLine("The Area of the Circle is:"+circle.area());
+    Console.Clear();
+    Console.WriteLine("Select a Shape:");
+    Console.WriteLine("1.Circle");
+    Console.WriteLine("2.Triangle");
+    Console.WriteLine("3.Square");
+    Console.WriteLine("4.Rectangle");
+    int x = Convert.ToInt32(Console.ReadLine());
+    switch (x)
+    {
+        case 1:
+            {
+                Circle circle = new Circle();
+                Console.WriteLine("Enter Z radius:");
+                //double r=Convert.ToDouble(Console.ReadLine());
+                //Circle circle = new Circle(r);
+                circle._rad = Console.ReadLine();
+                Console.WriteLine("Calculate:");
+                Console.WriteLine("1.Area");
+                Console.WriteLine("2.Perimeter");
+                int i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("The Area of the Circle is:" + circle.area());
                             break;
-                           }
-                        case 2:
-                           {
-                        Console.WriteLine("The Circumference of the Circle is:" + circle.perimeter());
-                        break;
-                           }
-                    }
-            break;
-        }
-    case 2:
-        {
-            Triangle triangle = new Triangle();
-            Console.WriteLine("Assuming Right angle triangle!");
-            Console.WriteLine("Enter Side A,B,C respectively:");
-            //double A = Convert.ToDouble(Console.ReadLine());
-            //double B = Convert.ToDouble(Console.ReadLine());
-            //double C = Convert.ToDouble(Console.ReadLine());
-            //Triangle triangle = new Triangle(A,B,C);
-            triangle._A = Console.ReadLine();
-            triangle._B = Console.ReadLine();
-            triangle._C = Console.ReadLine();
-
-            Console.WriteLine("Calculate:");
-            Console.WriteLine("1.Area");
-            Console.WriteLine("2.Perimeter");
-            int i = Convert.ToInt32(Console.ReadLine());
-            switch (i)
-            {
-                case 1:
-                    {
-                        Console.WriteLine("The Area of the Triangle is:" + triangle.area());
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.WriteLine("The Perimeter of the Triangle is:" + triangle.perimeter());
-                        break;
-                    }
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("The Circumference of the Circle is:" + circle.perimeter());
+                            break;
+                        }
+                }
+                break;
             }
-            break;
-        }
-    case 3:
-        {
-            Square square = new Square();
-            Console.WriteLine("Enter Z length:");
-            //double l=Convert.ToDouble(Console.ReadLine());
-            //Square square = new Square(l);
-            square._w = Console.ReadLine();
-
-            Console.WriteLine("Calculate:");
-            Console.WriteLine("1.Area");
-            Console.WriteLine("2.Perimeter");
-            int i = Convert.ToInt32(Console.ReadLine());
-            switch (i)
+        case 2:
             {
-                case 1:
-                    {
-                        Console.WriteLine("The Area of the Square is:" + square.area());
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.WriteLine("The Perimeter of the Square is:" + square.perimeter());
-                        break;
-                    }
-            }
-            break;
-        }
-    case 4: 
-        {
-            Rectangle rectangle = new Rectangle();
-            Console.WriteLine("Enter length and width respectively:");
-            //double l= Convert.ToDouble(Console.ReadLine());
-            //double w=Convert.ToDouble(Console.ReadLine());
-            //Rectangle rectangle = new Rectangle(l,w);
-            rectangle._l = Console.ReadLine();
-            rectangle._w = Console.ReadLine();
+                Triangle triangle = new Triangle();
+                Console.WriteLine("Assuming Right angle triangle!");
+                Console.WriteLine("Enter Side A,B,C respectively:");
+                //double A = Convert.ToDouble(Console.ReadLine());
+                //double B = Convert.ToDouble(Console.ReadLine());
+                //double C = Convert.ToDouble(Console.ReadLine());
+                //Triangle triangle = new Triangle(A,B,C);
+                triangle._A = Console.ReadLine();
+                triangle._B = Console.ReadLine();
+                triangle._C = Console.ReadLine();
 
-            Console.WriteLine("Calculate:");
-            Console.WriteLine("1.Area");
-            Console.WriteLine("2.Perimeter");
-            int i=Convert.ToInt32(Console.ReadLine());
-            switch (i)
+                Console.WriteLine("Calculate:");
+                Console.WriteLine("1.Area");
+                Console.WriteLine("2.Perimeter");
+                int i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("The Area of the Triangle is:" + triangle.area());
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("The Perimeter of the Triangle is:" + triangle.perimeter());
+                            break;
+                        }
+                }
+                break;
+            }
+        case 3:
             {
-                case 1:
-                    {
-                        Console.WriteLine("The Area of the Rectangle is:" + rectangle.area());
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.WriteLine("The Perimeter of the Rectangle is:" + rectangle.perimeter());
-                        break;
-                    }
-            }
-            break;
-        }
-}
+                Square square = new Square();
+                Console.WriteLine("Enter Z length:");
+                //double l=Convert.ToDouble(Console.ReadLine());
+                //Square square = new Square(l);
+                square._w = Console.ReadLine();
 
+                Console.WriteLine("Calculate:");
+                Console.WriteLine("1.Area");
+                Console.WriteLine("2.Perimeter");
+                int i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("The Area of the Square is:" + square.area());
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("The Perimeter of the Square is:" + square.perimeter());
+                            break;
+                        }
+                }
+                break;
+            }
+        case 4:
+            {
+                Rectangle rectangle = new Rectangle();
+                Console.WriteLine("Enter length and width respectively:");
+                //double l= Convert.ToDouble(Console.ReadLine());
+                //double w=Convert.ToDouble(Console.ReadLine());
+                //Rectangle rectangle = new Rectangle(l,w);
+                rectangle._l = Console.ReadLine();
+                rectangle._w = Console.ReadLine();
+
+                Console.WriteLine("Calculate:");
+                Console.WriteLine("1.Area");
+                Console.WriteLine("2.Perimeter");
+                int i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("The Area of the Rectangle is:" + rectangle.area());
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("The Perimeter of the Rectangle is:" + rectangle.perimeter());
+                            break;
+                        }
+                }
+                break;
+            }
+    }
+        i: Console.Write("Do you want to continue? Y/N ");
+            input = Console.ReadLine();
+            a = input.ToUpper();
+            if (a != "Y" && a != "N")
+            {
+                Console.WriteLine("Bad Input.Please try Again!");
+                goto i;
+            }
+            if (a == "N")
+            {
+                Console.WriteLine("Thanks!");
+                break;
+            }
+} while (a == "Y");
 
 
 
